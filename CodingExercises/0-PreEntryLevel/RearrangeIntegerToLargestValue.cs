d959b770976bb1dde9e02c3b3bfef3d8ec534c7e
Matt Hartzell <mhartzell@gmail.com>
2018-06-03 15:32:37 -0700

    Trying out first coding exercise.

---
+++ CodingExercises/0-PreEntryLevel/RearrangeIntegerToLargestValue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PMasta.Learning.CodingExercises._0_PreEntryLevel
+{
+    /// <summary>
+    /// Problem: Write a function that rearranges an integer into it's largest possible value.
+    /// </summary>
+    /// <remarks>Taken from https://www.codewars.com/kata/noobcode-01-supersize-me-dot-dot-dot-or-rather-this-integer/csharp </remarks>
+    /// <example>
+    /// Kata.SuperSize(123456) //654321
+    /// Kata.SuperSize(105) // 510
+    /// Kata.SuperSize(12) // 21
+    /// </example>
+    public class RearrangeIntegerToLargestValue
+    {
+        public static long SuperSizeMe(long value)
+        {
+            return Convert.ToInt64(
+                String.Join(
+                    string.Empty, 
+                    value.ToString().ToCharArray().OrderByDescending(c => Convert.ToInt64(c)).ToArray()));
+        }
+    }
+}
---
+++ Tests.Unit.CodingExercises/0-PreEntryLevel/PreEntryLevelFixture.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PMasta.Learning.CodingExercises._0_PreEntryLevel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PMasta.Learning.Tests.Unit.CodingExercises._0_PreEntryLevel
+{
+    /// <summary>
+    /// Tests related to the Pre-Entry Level coding exercises.
+    /// </summary>
+    [TestClass]
+    public class PreEntryLevelFixture
+    {
+        [TestMethod]
+        public void RearrangeIntegerToLargestValueSucceeds()
+        {
+            Assert.IsTrue(RearrangeIntegerToLargestValue.SuperSizeMe(1009876) == 9876100);
+            Assert.IsTrue(RearrangeIntegerToLargestValue.SuperSizeMe(123456) == 654321);
+            Assert.IsTrue(RearrangeIntegerToLargestValue.SuperSizeMe(105) == 510);
+            Assert.IsTrue(RearrangeIntegerToLargestValue.SuperSizeMe(12) == 21);
+            Assert.IsTrue(RearrangeIntegerToLargestValue.SuperSizeMe(9) == 9);
+        }
+    }
+}
