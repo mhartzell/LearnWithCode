9b153702ce7cf617d03a9ae74ed6bef24a796caf
Matt Hartzell <mhartzell@gmail.com>
2018-06-03 20:26:44 -0700

    Removing usings.

--- CodingExercises/0-PreEntryLevel/RemoveTrailingZeroes.cs
+++ CodingExercises/0-PreEntryLevel/RemoveTrailingZeroes.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PMasta.Learning.CodingExercises._0_PreEntryLevel
+﻿namespace PMasta.Learning.CodingExercises._0_PreEntryLevel
 {
     /// <summary>
     /// Get rid of any / all trailing zeros from a number.
