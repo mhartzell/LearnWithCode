5b7c14567fca411bd0490e2d644da6a0a9af0dc2
Matt Hartzell <mhartzell@gmail.com>
2018-06-05 09:17:20 -0700

    Implementing the Singleton creational pattern.

---
+++ DesignPatterns/Creational/Singleton/WeaponFactory.cs
@@ -0,0 +1,50 @@
+﻿namespace PMasta.Learning.DesignPatterns.Creational.Singleton
+{
+	/// <summary>
+	/// A class implementing the Single object creational pattern.
+	/// </summary>
+	/// <remarks>
+	/// For some objects, only one instance should exist in the system.  A domain service is a great example.  The 
+	/// Singleton pattern ensures a class has only one instance and also provides a global point of access to it.
+	/// In this example, there should only be on instance of <see cref="WeaponFactory"/>, regardless of the chosen 
+	/// implementation.
+	/// </remarks>
+	public class WeaponFactory : IWeaponFactory
+	{
+		/// <summary>
+		/// The Singleton instance.
+		/// </summary>
+		protected static WeaponFactory _instance;
+		
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <remarks>
+		/// Is marked as protected to allow subclasses to further specify the Singleton instance.  Could also be marked private.
+		/// </remarks>
+		protected WeaponFactory()
+		{
+
+		}
+
+		public static WeaponFactory Instance
+		{
+			// If no instance is created, make one.  Otherwise, return the instance.
+			get
+			{
+				return _instance ?? (_instance = new WeaponFactory());
+			}
+
+			protected set { }
+		}
+
+		/// <summary>
+		/// Some service the factory provides.
+		/// </summary>
+		/// <returns>Something.</returns>
+		public object MakeWeapon()
+		{
+			return new object();
+		}
+	}
+}
---
+++ Tests.Unit.DesignPatterns/Creational/SingletonFixture.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PMasta.Learning.DesignPatterns.Creational.Singleton;
+
+namespace PMasta.Learning.Tests.Unit.DesignPatterns.Creational
+{
+	/// <summary>
+	/// Tests related to the Singleton pattern.
+	/// </summary>
+	[TestClass]
+	public class SingletonFixture
+    {
+		[TestMethod]
+		public void ConstructSucceeds()
+		{
+			// Compilation error because the constructor is inaccessible
+			//WeaponFactory factory = new WeaponFactory();
+
+			WeaponFactory factory = WeaponFactory.Instance;
+
+			Assert.IsNotNull(factory);
+		}
+
+		[TestMethod]
+		public void MultipleInstancesAreEqualSucceeds()
+		{
+			WeaponFactory factory1 = WeaponFactory.Instance;
+			WeaponFactory factory2 = WeaponFactory.Instance;
+
+			Assert.ReferenceEquals(factory1, factory2);
+		}
+    }
+}
