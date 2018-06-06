d0ce6e70709ef2eea4588342e875277b82c2cbb0
Matt Hartzell <mhartzell@gmail.com>
2018-06-05 22:00:54 -0700

    Added an Entry Level coding exercise.

---
+++ CodingExercises/1-EntryLevel/DoubleAllCharacters.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace PMasta.LearnWithCode.CodingExercises._1_EntryLevel
+{
+    /// <summary>
+    /// Given a string, return a string in which each character (case-sensitive) is repeated once.
+    /// </summary>
+    public class DoubleAllCharacters
+    {
+        public static string DoubleChar(string input) => string.Join("", input.Select((c, output) => c.ToString() + c.ToString()));
+
+        // Alternate solutions
+        // public static string DoubleChar(string input) => string.Join("", s.Select(x => "" + x + x));
+        // public static string DoubleChar(string input) => s.Aggregate("", (str, c) => str + c + c);
+        // public static string DoubleChar(string input) => string.Concat(s.Select(x => $"{x}{x}"));
+        // public static string DoubleChar(string input) => string.Join("", s.Select(p=> $"{p}{p}"));
+        // public static string DoubleChar(string input) => Regex.Replace(s, ".", "$&$&");
+    }
+}
---
+++ Tests.Unit.CodingExercises/1-EntryLevel/EntryLevelFixture.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PMasta.LearnWithCode.CodingExercises._1_EntryLevel;
+
+namespace PMasta.LearnWithCode.Tests.Unit.CodingExercises._1_EntryLevel
+{
+    /// <summary>
+    /// Tests related to the Entry Level coding exercises.
+    /// </summary>
+    [TestClass]
+    public class EntryLevelFixture
+    {
+        [TestMethod]
+        public void DoubleAllCharactersSucceeds()
+        {
+            Assert.IsTrue(DoubleAllCharacters.DoubleChar("String") == "SSttrriinngg");
+            Assert.IsTrue(DoubleAllCharacters.DoubleChar("Hello World") == "HHeelllloo  WWoorrlldd");
+            Assert.IsTrue(DoubleAllCharacters.DoubleChar("1234!_ ") == "11223344!!__  ");
+        }
+    }
+}
