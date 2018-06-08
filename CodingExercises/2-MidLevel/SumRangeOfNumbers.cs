41e828e5be5361c4320dff1e799bbfafaebca276
Matt Hartzell <mhartzell@gmail.com>
2018-06-07 19:06:05 -0700

    Adding a Mid Level coding exercise.

---
+++ CodingExercises/2-MidLevel/SumRangeOfNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PMasta.LearnWithCode.CodingExercises._2_MidLevel
+{
+    public class SumRangeOfNumbers
+    {
+        public static int SumRange(int[] range)
+        {
+            int lower = range[0] > range[1] ? range[1] : range[0];
+            int upper = range[0] > range[0] ? range[0] : range[1];
+
+            return Enumerable.Range(lower, (upper - lower) + 1).Sum();
+        }
+    }
+}
---
+++ Tests.Unit.CodingExercises/2-MidLevel/SumRangeOfNumbersFixture.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PMasta.LearnWithCode.CodingExercises._2_MidLevel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PMasta.LearnWithCode.Tests.Unit.CodingExercises._2_MidLevel
+{
+    [TestClass]
+    public class SumRangeOfNumbersFixture
+    {
+        [TestMethod]
+        public void SumRangeSucceeds()
+        {
+            Assert.IsTrue(SumRangeOfNumbers.SumRange(new int[] { 1, 5 }) == 15);
+            Assert.IsTrue(SumRangeOfNumbers.SumRange(new int[] { 3, 7 }) == 25);
+        }
+    }
+}
